c72aa6f62cc592de49817c21a5eb762d71d3b367
Ciaran <ciaran.abrams@gmail.com>
2021-05-10 11:45:48 +0100

    fixed installer scripts
    1 - updated first action to point to correct script
    2 - added glfw3 to vcpkg install libs

--- veedBootrstrap.sh
+++ veedBootrstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-./bootstrap.sh
-./vcpkg.exe install libpq:x64-windows libpqxx:x64-windows boost:x64-windows ffmpeg:x64-windows cpprestsdk:x64-windows
+./bootstrap-vcpkg.sh
+./vcpkg.exe install libpq:x64-windows libpqxx:x64-windows boost:x64-windows ffmpeg:x64-windows cpprestsdk:x64-windows glfw3:x64-windows
 read
